f189649c0153da1ee8d8bf0f7dd5d9aaadfe5df0
josepumari <49406151+josepumari@users.noreply.github.com>
2019-04-11 14:59:20 -0300

    Update main.cpp

--- holamundo/main.cpp
+++ holamundo/main.cpp
@@ -1,3 +1,13 @@
+
+/*
+• Algoritmos y Estructuras de Datos
+• Curso k1051
+• Año de cursada, y cuatrimestre si corresponde. 2019 
+• Legajo: 1540579
+• Apellido pumari
+• Nombre jose
+*/
+
 #include <iostream>
 
 using namespace std;
